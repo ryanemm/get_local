7e2674998eb83a712bef3cb991f7b196d626d6b7
Mac <emersonryant@gmail.com>
2024-06-30 22:47:11 +0200

    logout function

--- lib/layouts/home/home_screen.dart
+++ lib/layouts/home/home_screen.dart
@@ -7,6 +7,7 @@ import 'package:get_local/layouts/profile/profile_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:load_switch/load_switch.dart';
 import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -18,10 +19,14 @@ class _HomeScreenState extends State<HomeScreen> {
   bool switchValue = false;
   String accoutType = "company";
   String companyId = "1";
+  String? email;
+  String? name;
+  String? surname;
 
   @override
   void initState() {
     super.initState();
+    getUserDetails();
     _controller = PersistentTabController(initialIndex: 0);
   }
 
@@ -30,10 +35,34 @@ class _HomeScreenState extends State<HomeScreen> {
     return !switchValue;
   }
 
+  Future getUserDetails() async {
+    SharedPreferences userDetails = await SharedPreferences.getInstance();
+    name = userDetails.getString("name");
+    email = userDetails.getString("email");
+
+    surname = userDetails.getString("surname");
+    print("Email: $email");
+    print("Name: $name");
+    print("Surname: $surname");
+  }
+
+  Future<void> logout() async {
+    SharedPreferences userDetails = await SharedPreferences.getInstance();
+    await userDetails.setString("email", "");
+    await userDetails.setString("name", "");
+    await userDetails.setString("surname", "");
+    await userDetails.setString("loggedIn", "false");
+    print("User logged out");
+  }
+
   List<Widget> _buildScreens() {
     return [
-      const Feed(),    
-      accoutType == "local" ? const ListingsScreen(accountType: "local") : ListingsScreenCompany(companyId: companyId,),
+      const Feed(),
+      accoutType == "local"
+          ? const ListingsScreen(accountType: "local")
+          : ListingsScreenCompany(
+              companyId: companyId,
+            ),
       NotificationsScreen(),
       const ProfileScreen(accountType: "local"),
     ];
@@ -50,19 +79,19 @@ class _HomeScreenState extends State<HomeScreen> {
       PersistentBottomNavBarItem(
         icon: const Icon(Icons.work),
         title: "Listings",
-        activeColorPrimary: Color.fromARGB(255, 31,69,77),
+        activeColorPrimary: Color.fromARGB(255, 31, 69, 77),
         inactiveColorPrimary: Colors.grey,
       ),
       PersistentBottomNavBarItem(
         icon: const Icon(Icons.notifications),
         title: "Notifications",
-        activeColorPrimary: Color.fromARGB(255, 31,69,77),
+        activeColorPrimary: Color.fromARGB(255, 31, 69, 77),
         inactiveColorPrimary: Colors.grey,
       ),
       PersistentBottomNavBarItem(
         icon: const Icon(Icons.person),
         title: "Profile",
-        activeColorPrimary: Color.fromARGB(255, 31,69,77),
+        activeColorPrimary: Color.fromARGB(255, 31, 69, 77),
         inactiveColorPrimary: Colors.grey,
       ),
     ];
@@ -71,64 +100,52 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(leading:    Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          children: [
-            Container(
-                        height: 32,
-                        width: 32,
-                        decoration: const BoxDecoration(
-                            borderRadius: BorderRadius.all(Radius.circular(25)),
-                            color: Colors.white),
-                            child: Padding(
-                              padding: const EdgeInsets.all(2.0),
-                              child: Image.asset("assets/images/logo_yellow.png"),
-                            ),
-                      ),
-                      
-          ],
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              Container(
+                height: 32,
+                width: 32,
+                decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(25)),
+                    color: Colors.white),
+                child: Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: Image.asset("assets/images/logo_yellow.png"),
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
-      centerTitle: true,
-      title:    Text(
-                  "GetLocal",
-                  style: GoogleFonts.aboreto(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold, color: Colors.white),
-                ),
-      actions: [   
-             
-                    LoadSwitch(
-                  height: 25,
-                  width: 60,
-                  value: switchValue,
-                  future: _getFuture,
-                  style: SpinStyle.material,
-                  onChange: (v) {
-                    switchValue = v;
-                    print('Value changed to $v');
-                    setState(() {});
-                  },
-                  onTap: (v) {
-                    print('Tapping while value is $v');
-                  },
-                ),
-                const SizedBox(width: 8),
-                const Icon(
-                  Icons.menu_rounded,
-                  color: Colors.white,
-                ), const SizedBox(width: 8),],
-      backgroundColor: Color.fromARGB(255, 31,69,77),
+        centerTitle: true,
+        title: Text(
+          "GetLocal",
+          style: GoogleFonts.aboreto(
+              fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+        actions: [
+          const SizedBox(width: 8),
+          GestureDetector(
+            onTap: () {
+              logout();
+            },
+            child: const Icon(
+              Icons.menu_rounded,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(width: 8),
+        ],
+        backgroundColor: Color.fromARGB(255, 31, 69, 77),
       ),
       body: Column(
         children: [
-          
           Container(
             height: 24,
-            color: Color.fromARGB(255, 31,69,77),
+            color: Color.fromARGB(255, 31, 69, 77),
             child: Container(
-            
               decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius:
@@ -142,7 +159,8 @@ class _HomeScreenState extends State<HomeScreen> {
               screens: _buildScreens(),
               items: _navBarsItems(),
               confineInSafeArea: true,
-              backgroundColor: Color.fromARGB(255, 0, 7, 45), // Default is Colors.white.
+              backgroundColor:
+                  Color.fromARGB(255, 0, 7, 45), // Default is Colors.white.
               handleAndroidBackButtonPress: true, // Default is true.
               resizeToAvoidBottomInset:
                   true, // This needs to be true if you want to move up the screen when the keyboard appears.
@@ -167,7 +185,6 @@ class _HomeScreenState extends State<HomeScreen> {
                   .style14, // Choose the nav bar style with this property.
             ),
           ),
-          
         ],
       ),
     );
