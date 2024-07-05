a7d05c0fb660d92c1ea7ca236cfb7224ac7d41c6
Mac <emersonryant@gmail.com>
2024-07-05 10:05:44 +0200

    company profile bio

--- lib/layouts/profile/profile_screen_company.dart
+++ lib/layouts/profile/profile_screen_company.dart
@@ -37,53 +37,109 @@ class _ProfileScreenCompanyState extends State<ProfileScreenCompany> {
                   child: Icon(Icons.person_2_outlined),
                 ),
                 SizedBox(width: 8),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Row(
-                      children: [
-                        Text(
-                          "Kosi Connect",
-                          style: GoogleFonts.montserrat(
-                              color: Color.fromARGB(255, 2, 50, 10),
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
                     Text(
-                      "Filtering Services",
+                      "Kosi Connect",
                       style: GoogleFonts.montserrat(
-                          color: Colors.black,
-                          fontSize: 16,
-                          fontWeight: FontWeight.normal),
+                          color: Color.fromARGB(255, 2, 50, 10),
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold),
                     ),
+                    SizedBox(
+                      width: 120,
+                    ),
+                    Icon(
+                      Icons.edit,
+                      color: Color.fromARGB(255, 0, 33, 7),
+                    )
                   ],
                 ),
                 Expanded(child: Container()),
               ],
             ),
             SizedBox(height: 16),
-            Text(
-              "21 Steenkamp Street, Del Jundor",
-              style: GoogleFonts.montserrat(
-                  color: Colors.black,
-                  fontSize: 16,
-                  fontWeight: FontWeight.normal),
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(16)),
+                  color: Color.fromARGB(255, 202, 214, 203)),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.location_pin,
+                        color: Color.fromARGB(255, 2, 36, 10),
+                      ),
+                      Text(
+                        "21 Steenkamp Street, Del Jundor",
+                        style: GoogleFonts.montserrat(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontWeight: FontWeight.normal),
+                      ),
+                    ],
+                  ),
+                  Text(
+                    "     Witbank",
+                    style: GoogleFonts.montserrat(
+                        color: Colors.black,
+                        fontSize: 16,
+                        fontWeight: FontWeight.normal),
+                  ),
+                  Text(
+                    "     Mpumalanga",
+                    style: GoogleFonts.montserrat(
+                        color: Colors.black,
+                        fontSize: 16,
+                        fontWeight: FontWeight.normal),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "⦿ Filtering ",
+                        style: GoogleFonts.montserrat(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "  ⦿ Stone Crushing ",
+                        style: GoogleFonts.montserrat(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  Text(
+                    "⦿ Geochemical Analysis ",
+                    style: GoogleFonts.montserrat(
+                        color: Colors.black,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
             ),
-            Text(
-              "Witbank",
-              style: GoogleFonts.montserrat(
-                  color: Colors.black,
-                  fontSize: 16,
-                  fontWeight: FontWeight.normal),
-            ),
-            Text(
-              "Mpumalanga",
-              style: GoogleFonts.montserrat(
-                  color: Colors.black,
-                  fontSize: 16,
-                  fontWeight: FontWeight.normal),
+            SizedBox(height: 16),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Kosi Connect is a leading provider of comprehensive stone crushing and geochemical analysis services. With years of industry experience and state-of-the-art technology, we deliver high-quality solutions for mining, construction, and environmental projects.",
+                style: GoogleFonts.montserrat(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.normal),
+                textAlign: TextAlign.justify,
+              ),
             )
           ],
         ),
